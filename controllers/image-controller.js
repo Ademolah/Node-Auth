c70c936f7bb766a841a26219757bf4f922ed1ca5
Charles <adeakinyemi129@gmail.com>
2025-03-16 06:42:07 +0100

    fnished image upload controller

--- controllers/image-controller.js
+++ controllers/image-controller.js
@@ -1,13 +1,36 @@
 const Image = require('../models/image.js')
+const {uploadToCloudinary} = require('../helpers/cloudinaryHelpers.js')
 
 const uploadImage = async (req, res)=>{
+
     try {
+
+        //check if file is upload is missing
         if(!req.file){
-            res.status(400).json({
+            return res.status(400).json({
                 success: false,
                 message: 'No file uploaded, please upload an image file'
             })
         }
+
+        //upload image to cloudinary
+        const {url, publicId} = await uploadToCloudinary(req.file.path)
+
+        //store the url and publicId in database
+        const newlyUploadedImage = new Image({
+            url,
+            publicId,
+            uploadedBy: req.userInfo.userId
+        })
+
+        await newlyUploadedImage.save()
+
+        res.status(201).json({
+            success: true,
+            message: 'Image uploaded successfully',
+            image: newlyUploadedImage
+        })
+
         
     } catch (error) {
         console.error("Error ", error)
